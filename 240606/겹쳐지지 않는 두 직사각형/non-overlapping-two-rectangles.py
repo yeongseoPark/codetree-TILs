533099697d876b76b9747f37b148fef88076a3fb
YSEO <1park4170@gmail.com>
2024-06-06 19:01:46 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 424ms, Memory: 29MB, Status: Passed - CodeTree

--- 240606/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240606/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -2,57 +2,41 @@ n, m = map(int, input().split())
 
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-max_row = n
-max_col = m
-
 def in_range(i, j):
     return 0 <= i < n and 0 <= j < m
 
 def not_duplicated(i, r, j, c, row_start, row_end, col_start, col_end):
-    if row_start <= i <= row_end or row_start <= i + r <= row_end or col_start <= j <= col_end or col_start <= j + c <= col_end:
-        return False
-    
-    return True
+    if (i > row_end or i + r < row_start) or (j > col_end or j + c < col_start):
+        return True
+    return False
 
 def get_sum(row_start, row_end, col_start, col_end):
     tmp = 0
     for i in range(row_start, row_end + 1):
         for j in range(col_start, col_end + 1):
             tmp += grid[i][j]
-    
     return tmp
 
 def second_rec(row_start, row_end, col_start, col_end):
     tmp = float('-inf')
     for i in range(n):
         for j in range(m):
-            for r in range(max_row):
-                for c in range(max_col):
+            for r in range(n):
+                for c in range(m):
                     if in_range(i + r, j + c) and not_duplicated(i, r, j, c, row_start, row_end, col_start, col_end):
-                        row_range = (i, i+r)
-                        col_range = (j, j+c) 
-
-                        mine    = get_sum(row_range[0], row_range[1], col_range[0], col_range[1])
-
+                        mine = get_sum(i, i + r, j, j + c)
                         tmp = max(tmp, mine) 
-    
     return tmp
 
 ans = float('-inf')
 for i in range(n):
     for j in range(m):
-        for r in range(max_row):
-            for c in range(max_col):
+        for r in range(n):
+            for c in range(m):
                 if in_range(i + r, j + c):
-                    row_range = (i, i+r)
-                    col_range = (j, j+c) 
-
-                    mine    = get_sum(row_range[0], row_range[1], col_range[0], col_range[1])
-                    another = second_rec(row_range[0], row_range[1], col_range[0], col_range[1])
-
-                    if another == float('-inf'):
-                        ans = max(ans, mine)
-                    else:
-                        ans = max(ans, mine + another) 
+                    mine = get_sum(i, i + r, j, j + c)
+                    another = second_rec(i, i + r, j, j + c)
+                    if another != float('-inf'):
+                        ans = max(ans, mine + another)
 
 print(ans)
