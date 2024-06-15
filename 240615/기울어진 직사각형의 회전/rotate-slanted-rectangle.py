32178c9cfe8a8dce28f16ac6a2a5813c982a89ad
YSEO <1park4170@gmail.com>
2024-06-15 16:22:11 +0900

    Title: 기울어진 직사각형의 회전, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240615/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 240615/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -22,7 +22,7 @@ def rotate(row, col, m1, m2, m3, m4, direction):
         temp = grid[bottom_row][bottom_col]
 
         for i in range(m1-1):
-            grid[bottom_row + (i * right_up[0])][bottom_col + (i * right_up[1])] = grid[bottom_row + ((i+1) * right_up[0])][bottom_col + ((i+1) * right_up[1])] 
+            grid[bottom_row + (i * right_up[0])][bottom_col + (i * right_up[1])] = grid[bottom_row + ((i+1) * right_up[0])][bottom_col + ((i+1) * right_up[1])]
 
         for i in range(m2-1):
             grid[right_row + (i * left_up[0])][right_col + (i * left_up[1])] = grid[right_row + ((i+1) * left_up[0])][right_col + ((i+1) * left_up[1])]
@@ -32,8 +32,8 @@ def rotate(row, col, m1, m2, m3, m4, direction):
 
         for i in range(m4-1):
             grid[left_row + (i * right_down[0])][left_col + (i * right_down[1])] = grid[left_row + ((i+1) * right_down[0])][left_col + ((i+1) * right_down[1])]
-        
-        grid[left_row + ((m4-1) * right_down[0])][left_col + ((m4-1) * right_down[1])] = temp
+
+        grid[left_row + (m4-1) * right_down[0]][left_col + (m4-1) * right_down[1]] = temp
 
     else: # 반시계 방향 회전
         temp = grid[bottom_row][bottom_col]
@@ -43,14 +43,14 @@ def rotate(row, col, m1, m2, m3, m4, direction):
 
         for i in range(m3-1):
             grid[left_row + (i * left_down[0])][left_col + (i * left_down[1])] = grid[left_row + ((i+1) * left_down[0])][left_col + ((i+1) * left_down[1])]
-        
+
         for i in range(m2-1):
-            grid[top_row + (i * left_up[0])][top_col + (i * left_up[1])] = grid[top_row + ((i+1) * left_up[0])][top_col + ((i+1) * left_up[1])] 
-        
+            grid[top_row + (i * left_up[0])][top_col + (i * left_up[1])] = grid[top_row + ((i+1) * left_up[0])][top_col + ((i+1) * left_up[1])]
+
         for i in range(m1-1):
             grid[right_row + (i * right_up[0])][right_col + (i * right_up[1])] = grid[right_row + ((i+1) * right_up[0])][right_col + ((i+1) * right_up[1])]
 
-        grid[right_row + ((m1-1) * right_up[0])][right_col + ((m1-1) * right_up[1])] = temp
+        grid[right_row + (m1-1) * right_up[0]][right_col + (m1-1) * right_up[1]] = temp
 
 for row in range(1, n+1):
     cur_list = list(map(int, input().split()))
