a28edc00069cd3e234a6eba4647beba6c7afcec0
YSEO <1park4170@gmail.com>
2024-06-16 14:39:35 +0900

    Title: 1차원 젠가, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240616/1차원 젠가/jenga-1d.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+arr = []
+for _ in range(n):
+    arr.append(int(input()))
+
+arr = [0] + arr[::-1]
+first  = tuple(map(int, input().split()))
+second = tuple(map(int, input().split()))
+
+def reduce(start, end):
+    temp = []
+
+    for i in range(1, len(arr)):
+        if i < start or i > end:
+            temp.append(arr[i])
+    
+    return [0] + temp
+
+arr = reduce(first[0], first[1])
+arr = reduce(second[0], second[1])
+
+print(len(arr) -1)
+for i in range(len(arr)-1, 0, -1):
+    print(arr[i])
