d0d73ec69af2dcc13a2bb85a3060c563b621fe95
YSEO <1park4170@gmail.com>
2024-06-25 12:42:24 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240625/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -0,0 +1,40 @@
+n, r, c = map(int, input().split())
+
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+r -= 1
+c -= 1
+
+def is_valid(r, c):
+    return 0 <= r < n and 0 <= c < n
+
+movement = []
+
+while True:
+    moved = False
+
+    dr = [0, 0, -1, 1]
+    dc = [1, -1, 0, 0]
+
+    movement.append(grid[r][c])
+    cur_num = grid[r][c]
+
+    for i in range(4):
+        nr = r + dr[i]
+        nc = c + dc[i]
+
+        if is_valid(nr, nc):
+            if grid[nr][nc] > cur_num:
+                max_pos = (nr , nc)
+                moved = True
+
+    r = max_pos[0]
+    c = max_pos[1]
+    if not moved:
+        break
+
+for i in movement:
+    print(i, end = " ")
