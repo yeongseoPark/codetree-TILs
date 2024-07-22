8c1d59a21a559ed9952fc4f827e49fa3ffd21e80
YSEO <1park4170@gmail.com>
2024-07-22 13:17:19 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 171ms, Memory: 26MB, Status: Passed - CodeTree

--- 240722/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 240722/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -1,60 +1,72 @@
-n, m, t = map(int, input().split())
+n, m , t = map(int, input().split())
 
-grid = [
-    [0 for _ in range(n+1)]
-]
-
+a = [[0] * (n+1)]
 for _ in range(n):
-    grid.append([0] + list(map(int, input().split())))
+    a.append([0] + list(map(int, input().split())))
 
 count = [
     [0 for _ in range(n+1)]
     for _ in range(n+1)
 ]
 
-for _ in range(m):
-    r, c = map(int, input().split())
-    count[r][c] += 1 
-
 next_count = [
     [0 for _ in range(n+1)]
     for _ in range(n+1)
 ]
 
-dx = [-1, 1, 0, 0] # 상 하 좌 우
-dy = [0, 0, -1, 1]
+def in_range(x, y):
+    return 1 <= x and x <= n and 1 <= y and y <= n
 
-def in_range(r, c):
-    return 1 <= r <= n and 1 <= c <= n 
+def get_max_neighbor_pos(curr_x, curr_y):
+    dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+    max_num, max_pos = 0, (0, 0)
+
+    for dx, dy in zip(dxs, dys):
+        next_x, next_y = curr_x + dx, curr_y + dy
+
+        if in_range(next_x, next_y) and a[next_x][next_y] > max_num:
+            max_num = a[next_x][next_y]
+            max_pos = (next_x, next_y)
+    
+    return max_pos
+
+def move(x ,y):
+    next_x, next_y = get_max_neighbor_pos(x, y)
+    next_count[next_x][next_y] += 1
+
+def move_all():
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            next_count[i][j] = 0 
+
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if count[i][j] == 1:
+                move(i, j) 
+    
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            count[i][j] = next_count[i][j]
+
+def remove_duplicate_marbles():
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if count[i][j] >= 2:
+                count[i][j] = 0
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    count[x][y] = 1
 
 for _ in range(t):
-    for r in range(1, n+1):
-        for c in range(1, n+1):
-            if count[r][c] != 0:
-                cur_val = grid[r][c]
-                moved = False
-                for i in range(4):
-                    nr = r + dx[i]
-                    nc = c + dy[i]
-                    if in_range(nr, nc) and grid[nr][nc] > cur_val:
-                        next_count[nr][nc] += count[r][c]
-                        moved = True
-                if not moved:
-                    next_count[r][c] += count[r][c]
-                
-    for i in range(n+1):
-        for j in range(n+1):
-            if next_count[i][j] >= 2:
-                next_count[i][j] = 0
-    
-    for i in range(n+1):
-        for j in range(n+1):
-            count[i][j] = next_count[i][j]
-            next_count[i][j] = 0
+    move_all()
+
+    remove_duplicate_marbles() 
 
 ans = 0
-for i in range(n+1):
-    for j in range(n+1):
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
         ans += count[i][j]
 
 print(ans)
